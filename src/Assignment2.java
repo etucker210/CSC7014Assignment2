cc867b55850290d6d438e9fba32f66ff907e6fb8
Elijah <etucker210@gmail.com>
2024-09-14 11:55:02 -0400

    Create the assignment class

---
+++ src/Assignment2.java
@@ -0,0 +1,22 @@
+/* ***********************************************************************
+ *
+ * File Name: Assignment2.java
+ * Author:    Elijah Tucker
+ * Last Edit: 14 Sep 2024
+ *
+ * Prompt:    Write a program (java, python, or C++) that reads a series
+ *            of integers from the user and prints the sum of all even
+ *            numbers, and the product of all odd numbers entered. The
+ *            program should continue reading numbers until the user
+ *            enters a negative number.
+ *
+ * Change Log:
+ *
+ * Date & Time         | Description
+ * --------------------|-----------------------------
+ * 14 Sep 2024 11:54AM | Create the assignment class
+ *
+ ************************************************************************/
+
+public class Assignment2 {
+}
