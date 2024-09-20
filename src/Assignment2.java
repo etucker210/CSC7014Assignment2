8ac6b3dddb471c12ce5cd5ff2912dac28ebd2cd7
Elijah A. Tucker <etucker210@users.noreply.github.com>
2024-09-20 16:24:49 -0400

    finish the main of the assignment and test

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -17,8 +17,10 @@
  * 14 Sep 2024 11:54AM | Create the assignment class
  * 14 Sep 2024 12:29PM | Create the main function
  * 14 Sep 2024 12:37PM | Make the calculateProduct function
- *
+ * 14 Sep 2024         | Make the calculateSum function
+ * 19 Sep 2024  7:32PM | finish the main of the assignment and test
  ************************************************************************/
+import java.util.Scanner; //the class for input
 
 public class Assignment2 {
 
@@ -33,6 +35,57 @@ public class Assignment2 {
      *
      ********************************************************************/
     public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        int newValue;
+        int product = 0;
+        int sum = 0;
+
+        //Give the instructions to the user
+        System.out.println("This program takes an input from the user and" +
+                " sums all even numbers and multiplies all odd numbers." +
+                " A negative number will terminate the program.");
+
+        System.out.print("Enter an integer: ");
+
+        //get the first number
+        newValue = input.nextInt();
+
+        // If the number is positive keep going
+        while ( newValue >= 0 ) {
+
+            //If the number is even add the number to the sum otherwise
+            //calculate the product
+            if (newValue % 2 == 0) {
+
+                sum = calculateSum(sum, newValue);
+
+            } else {
+
+                // if the odd number is the fist one, set that as the
+                // first product otherwise multiply the last product
+                // with the next number
+                if (product == 0) {
+
+                    product = newValue;
+
+                } else {
+
+                    product = calculateProduct(product, newValue);
+
+                }
+            }
+
+            //prompt the user for the next number and get it from the user
+            System.out.print("Enter an integer (a negative number will terminate the program): ");
+            newValue = input.nextInt();
+
+        }
+
+        // tell the user the end product and the end sum
+        System.out.println("The product of the odd numbers is " + product + ".");
+        System.out.println("The sum of the even numbers is " + sum + ".");
 
     }
 
